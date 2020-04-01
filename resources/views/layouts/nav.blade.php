0dc1970435a6938c5262ce1ab4aba8d94e630468
Sherwin de Jesus <sherwin.dejesus@acidgreen.com.au>
2020-04-02 07:00:01 +0800

    Added Incoming Message menu

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -23,6 +23,9 @@
                           <li class="nav-item">
                             <a class="nav-link" href="/broadcasts-list">Broadcast Messages</a>
                           </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="/broadcasts-list">Incomming Messages</a>
+                          </li>
                       </div>
                 </ul>
                 @endif

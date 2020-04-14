7a72b9ad0b720db513de6a04399132a7dc29e4ca
Sherwin de Jesus <sherwin.dejesus@acidgreen.com.au>
2020-04-14 21:14:47 +0800

    added query caching

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,43 +13,55 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/regions', function(){
-    return \App\Regions::all();
+Route::get('/regions', function () {
+    return cache()->remember('regions', 60 * 60 * 24 * 30, function () {
+        return \App\Regions::all();
+    });
 });
 
 
-Route::get('/lgus/{regionShortName}', function($regionShortName){
-    return \App\LGU::where('region_short_name', $regionShortName)->get();
+Route::get('/lgus/{regionShortName}', function ($regionShortName) {
+    return cache()->remember('/lgus/{regionShortName}', 60 * 60 * 24 * 30, function () use ($regionShortName) {
+        return \App\LGU::where('region_short_name', $regionShortName)->get();
+    });
 });
 
-Route::get('/news/{lgu_id}', function($lgu_id){
-    return \App\News::where('lgu_id', $lgu_id)
-                    ->where('status', 'published')
-                    ->where('posting_date', '<=', now())
-                    ->latest('posting_date')
-                    ->limit(10)
-                    ->get();
+Route::get('/lgu/{id}', function ($id) {
+    return cache()->remember('/lgu/{id}', 60 * 60 * 24, function () use ($id) {
+        return \App\LGU::find($id);
+    });
 });
 
-Route::get('/events/{lgu_id}', function($lgu_id){
-    return \App\Events::where('lgu_id', $lgu_id)
-                    ->where('event_from', '>=', date('Y-m-d H:i:s'))
-                    ->oldest('event_from')
-                    ->limit(20)
-                    ->get();
+Route::get('/news/{lgu_id}', function ($lgu_id) {
+    return cache()->remember('/news/{lgu_id}', 60, function () use ($lgu_id) {
+        return \App\News::where('lgu_id', $lgu_id)
+            ->where('status', 'published')
+            ->where('posting_date', '<=', now())
+            ->latest('posting_date')
+            ->limit(10)
+            ->get();
+    });
 });
 
-Route::get('/bcastmsg/{lgu_id}', function($lgu_id){
-    return \App\Broadcast::where('lgu_id', $lgu_id)
-                    ->oldest('broadcast_on')
-                    ->limit(3)
-                    ->get();
+Route::get('/events/{lgu_id}', function ($lgu_id) {
+    return cache()->remember('/events/{lgu_id}', 60, function () use ($lgu_id) {
+        return \App\Events::where('lgu_id', $lgu_id)
+            ->where('event_from', '>=', date('Y-m-d H:i:s'))
+            ->oldest('event_from')
+            ->limit(10)
+            ->get();
+    });
+});
+
+Route::get('/bcastmsg/{lgu_id}', function ($lgu_id) {
+    return cache()->remember('/bcastmsg/{lgu_id}', 60, function () use ($lgu_id) {
+        return \App\Broadcast::where('lgu_id', $lgu_id)
+            ->oldest('broadcast_on')
+            ->limit(3)
+            ->get();
+    });
 });
 
 Route::post('/send_message', 'MessengerController@send');
 
 Route::post('/register_app_user', 'AppUsersController@register');
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
